dc17621945f357777dcd5c90e93eb768a5f8b12c
bitla <bitla.corp@gmail.com>
2018-10-31 22:06:52 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class controller {
+
+	public controller() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/welcome")
+	public String method1() {
+		return "Hello WOrld example";
+	}
+
+}
